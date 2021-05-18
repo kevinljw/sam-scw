41ebc166a5308c8ef5d43d7041104498299e82f0
evin92 <evin92@gamil.com>
2021-05-18 21:58:25 +0800

    pre sum

---
+++ Samsung_Spring_Campaign_files/cny.js
@@ -0,0 +1,427 @@
+var script = function () {
+
+    var $body = $('html,body');
+    var winH = $(window).height();
+    var winW = $(window).outerWidth();
+    // var docH  = $(document).height();
+    var $win = $(window);
+    var headerH = $('.myheader2').height();
+
+    // console.log("headerH2:"+headerH+"headerH1:"+$('.myheader').height());
+
+    var $sec = $('.sec');
+    var hash = location.hash,
+href;
+
+    AOS.init({
+        offset: 150,
+        duration: 800,
+        delay: 0,
+        easing: 'ease-in-out-sine',
+        anchorPlacement: 'top-bottom',
+    });
+
+    /* page scroll */
+    var thispage = 0;
+    var pageT = [],
+        $sec = $('.sec'),
+        $main = $('.wrapper'),
+        // _mainTOP = $main.offset().top,
+        ah = $main.outerHeight(true),
+        pageName = ['kv', 'main', 'z-series', 's21', 'n20', 'tab-s7','tab-a7', 'video', 'moresp', 'buy'];
+
+    var _thispagescroll = pageName.indexOf(hash);
+
+    var go = 0;
+
+    function swipergo2() {
+        var _scroll = $win.scrollTop();
+        var _sec8pos = $('.sec8').offset().top;
+
+        // if (go == 0) {
+        //     if (_scroll >= _sec8pos - 500) {
+        //         $('.sec8 .title').addClass('go');
+        //         $('.sec8 .title').css("display", "block");
+        //         go++;
+        //     }
+        // }
+    }
+
+    $win.scroll(swipergo2);
+
+
+
+
+    $('.btn_next').click(function () {
+
+        $('.mainIframe').each(function () {
+            this.contentWindow.postMessage('{"event": "command", "func": "stopVideo", "args": ""}', '*');
+        });
+
+    });
+
+
+    $('.btn_prev').click(function () {
+        $('.mainIframe').each(function () {
+            this.contentWindow.postMessage('{"event": "command", "func": "stopVideo", "args": ""}', '*');
+        });
+
+    });
+
+
+    $('.btnback').click(function () {
+        $('html,body').animate({ scrollTop: 0 }, 333);
+    });
+
+
+    /* header fix ---------------------------------------------------*/
+
+    var _sec2pos = $('.sec2').offset().top;
+
+    function headerfix() {
+        var _scroll = $win.scrollTop();
+        // console.log(_scroll, _sec2pos)
+        if (_scroll >= _sec2pos / 3) {
+            $('.myheader2').fadeIn('fast');
+        } else {
+            $('.myheader2').fadeOut('fast');
+        }
+    }
+
+    $win.scroll(headerfix);
+
+
+    // 重設
+    function navde() {
+
+        $('.mainnav').find('li').removeClass('open');
+
+        // $('.subnav').hide();
+
+        $('.burger').attr('toggle', '0');
+        $('.burger').removeClass('isclick');
+
+        $('.mainnav').css('height', '');
+        $('.myheader').css('height', '');
+
+        TweenMax.to('.myheader', .1, {ease: Quart.easeOut });
+
+        if (winW <= 768) {
+            $('.mainnav').hide();
+        } else {
+            $('.mainnav').show();
+        }
+    }
+    navde();
+
+    // 主選單
+    function navfunc() {
+        var navopen = $('.burger').attr('toggle');
+        if (navopen == 0) {
+            TweenMax.to('.myheader', .1, {ease: Quart.easeOut });
+
+            $('.mainnav').slideDown();
+
+            $('.burger').attr('toggle', '1');
+            $('.burger').addClass('isclick');
+
+        } else if (navopen == 1) {
+
+            $('.mainnav').css('height', '');
+            $('.myheader').css('height', '11vw');
+            TweenMax.to('.myheader', .5, {delay: 0, ease: Quart.easeOut });
+            $('.mainnav').slideUp();
+
+            $('.burger').attr('toggle', '0');
+            $('.burger').removeClass('isclick');
+        }
+    }
+    $('.burger').click(navfunc);
+
+
+    /* nav scroll --------------------------------------------*/
+
+    /* page scroll */
+    var thispage = 0;
+    var pageT = [],
+        $sec = $('.sec'),
+        $main = $('.wrapper'),
+        ah = $main.outerHeight(true);
+
+
+    var $navbtn = $('.navbtn');
+
+    var pageReset = function () {
+        pageT = [];
+        winW = $win.outerWidth();
+        ah = $main.outerHeight(true);
+        $sec.each(
+            function (i, obj) {
+                var t = $(obj).offset().top;
+                pageT.push(t);
+                if (i == $sec.length - 1) { pageT.push(ah); }
+            }
+        );
+    }
+
+    var menuselect = function () {
+        $navbtn.removeClass('select');
+        pageReset();
+
+        if (thispage >= 1 && thispage < 6) {
+            $('.navbtn[data-page="' + 2 + '"]').addClass('select');
+        }
+        else {
+            $('.navbtn[data-page="' + thispage + '"]').addClass('select');
+        }
+        // $(document).ready(function () {
+        //     console.log($("li.select span").html())
+        // });
+    }
+
+    var menuscroll = function (n) {
+        var _pos;
+        thispage = n;
+        pageReset();
+        $body.animate({
+            scrollTop: pageT[thispage] - headerH - 50
+        }, 600);
+
+    }
+    menuselect();
+
+    var scrollPage = function (wt, whelf) {
+        winH = $win.height();
+        winW = $win.outerWidth();
+        wt = $win.scrollTop();
+
+
+        if (winW < 768) {
+            whelf = winH / 6;
+        }
+        else {
+            whelf = winH / 3;
+        }
+        // _mainTOP = $main.offset().top;
+
+        //        console.log("width:"+winW+"H:"+winH+" "+whelf);
+
+        for (var p = 0; p < $sec.length; p++) {
+            if (wt > pageT[p] - whelf && wt < pageT[p + 1] - whelf) {
+                thispage = p;
+            }
+        }
+
+        menuselect();
+
+        href = pageName[thispage];
+        window.location.hash = href;
+    }
+
+    var scrollthispage = function () {
+        _thispagescroll = pageName.indexOf(hash.substr(1));
+
+
+        pageReset();
+
+        var topH = $('.myheader2').height();
+
+        if (_thispagescroll != -1) {
+            thispage = pageName.indexOf(hash.substr(1));
+
+            pageReset();
+
+            if (thispage == 0) {
+                TweenMax.to('body,html', .8, { scrollTop: 0, ease: Quart.easeOut });
+            } else {
+                TweenMax.to('body,html', .8, { scrollTop: pageT[thispage] - topH, ease: Quart.easeOut });
+
+            }
+
+            menuselect();
+            href = pageName[thispage];
+            window.location.hash = href;
+            pagescroll = false;
+        } else {
+
+            TweenMax.to('body,html', .8, { scrollTop: 0, ease: Quart.easeOut });
+        };
+
+    }
+    setTimeout(function () {
+        scrollthispage()
+    }, 600);
+
+    $navbtn.click(
+        function () {
+            var n = $(this).data('page');
+            navde();
+            menuscroll(n);
+        }
+    );
+
+
+    /* resize
+    ------------------------------------------------------------*/
+
+    var _resize = function () {
+
+        winW = $win.outerWidth();
+        winH = $win.height();
+        docH = $(document).height();
+        _scroll = $(window).scrollTop();
+        scrollBottom = $(window).scrollTop() + $(window).height();
+        headerH = $('.myheader2').height();
+
+        navde();
+
+    }
+    $win.scroll(scrollPage);
+    _resize();
+    $win.resize(function () {
+
+        // _resize();
+
+    });
+
+
+
+
+
+    var windowHeight = $(window).height();
+
+    // Resize Event
+    $(window).resize(function () {
+
+        // Check window width has actually changed and it's not just iOS triggering a resize event on scroll
+        if ($(window).width() != windowHeight) {
+
+            // Update the window width for next time
+            windowHeight = $(window).width();
+
+            // Do stuff here
+            _resize();
+            // if (pagescroll || windowHeight > 768) scrollthispage();
+
+        }
+
+    });
+}
+
+$(function () {
+
+    script();
+
+    $('.gb-footer-2019__btn-top').click(function () {
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+        $body.animate({
+            scrollTop: 0
+        }, 600);
+    })
+    // var stopVideo = function (element) {
+    //     var iframe = element.querySelector('iframe');
+    //     var video = element.querySelector('video');
+    //     if (iframe !== null) {
+    //         var iframeSrc = iframe.src;
+    //         iframe.src = iframeSrc;
+    //     }
+    //     if (video !== null) {
+    //         video.pause();
+    //     }
+    // };
+    // $('#demo li').click(stopVideo);
+    // $('#demo .arrow-left').click(stopVideo);
+    // $('#demo .arrow-right').click(stopVideo);
+});
+
+$(document).ready(function () {
+    var taggingFunc = function() {
+        _lt('send', 'cv', {type: '2021Spring_BTN'},['d32026ff-0c57-4140-9db5-5bba1f58fc94']);
+        
+        fbq('track', 'IM_CUSTOM',{product_id:'2021SPRINGCAMPAIGN',});
+        
+        gtag('event', 'conversion', {'allow_custom_scripts': true,'send_to': 'DC-9052290/2021000/td20200+standard'});
+        
+        window.dotq = window.dotq || [];
+        window.dotq.push(
+        {
+          'projectId': '10000',
+          'properties': {
+            'pixelId': '406503',
+            'qstrings': {
+              'et': 'custom',
+              'ea': '406503button1_20210331'
+            }
+        } } );
+    };
+    var taggingFuncLogin = function() {
+        _lt('send', 'cv', {type: '2021Spring_BTN'},['d32026ff-0c57-4140-9db5-5bba1f58fc94']);
+        
+        fbq('track', 'IM_CUSTOM',{product_id:'2021SPRINGCAMPAIGN',});
+        
+        gtag('event', 'conversion', {'allow_custom_scripts': true,'send_to': 'DC-9052290/2021000/td20200+standard'});
+        
+        window.dotq = window.dotq || [];
+        window.dotq.push(
+        {
+          'projectId': '10000',
+          'properties': {
+            'pixelId': '406503',
+            'qstrings': {
+              'et': 'custom',
+              'ea': '406503button2_20210331'
+            }
+        } } );
+    };
+    
+    $( '#buy_now_zflod2' ).click(taggingFunc);
+    $( '#buy_now_s21' ).click(taggingFunc);
+    $( '#buy_now_note20' ).click(taggingFunc);
+    $( '#buy_now_tab_s7' ).click(taggingFunc);
+    $( '#buy_now_buds_pro' ).click(taggingFunc);
+    $( '#buy_now_watch' ).click(taggingFunc);
+    //sec3
+    $( '#z_series-buy_now_zflod2' ).click(taggingFunc);
+    $( '#z_series-log_in' ).click(taggingFuncLogin);
+//    $( '#z_series-buy_ses' ).click(taggingFunc);
+    //sec4
+    $( '#s21-buy_now' ).click(taggingFunc);
+    $( '#s21-log_in' ).click(taggingFuncLogin);
+//    $( '#s21-buy_ses' ).click(taggingFunc);
+    //sec 5
+    $( '#n20-buy_now' ).click(taggingFunc);
+    $( '#n20-log_in' ).click(taggingFuncLogin);
+//    $( '#n20-buy_ses' ).click(taggingFunc);
+    //sec 6
+    $( '#tab-s7-buy_now').click(taggingFunc);
+    $( '#tab-s7-log_in').click(taggingFuncLogin);
+//    $( '#tab-s7-buy_ses').click(taggingFunc);
+    
+    $( '#tab-a7-buy_now').click(taggingFunc);
+    // sec 8
+//    $( '#moresp-learn_more_togo' ).click(taggingFunc);
+//    $( '#moresp-learn_more_roadshow' ).click(taggingFunc);
+//    $( '#moresp-learn_more_workshop' ).click(taggingFunc);
+    //sec 9
+    $( '#buy-buy_now_left' ).click(taggingFunc);
+    $( '#buy-buy_now_right' ).click(taggingFunc);
+//    $( '#buy-learn_more' ).click(taggingFunc);
+    
+    $("#youtube-arrow-right,#youtube-arrow-left").click(function(){
+        $('#demo.carousel iframe').each(function(){
+            $(this)[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+          });
+    });
+});
+
+$(document).ready(function () {
+    $('#carouselControls').carousel({
+        interval: NaN
+    });
+});
+
+$(document).ready(function () {
+    $('#demo').carousel({
+        interval: NaN
+    });
+});
